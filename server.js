13c302702a1692276f1be0473fca94f18845a129
Drumzminister <ishukpong418@gmail.com>
2019-11-24 18:15:21 +0100

    fixed server not returning body data

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
-const http = require('http');
-const app = require('./app');
+var express = require("express");
+var bodyParser = require('body-parser');
+
+const routes = require('./routes');
+
+var app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.set('port', process.env.PORT || 3000);
-const server = http.createServer(app);
 
-server.listen(process.env.PORT || 3000);
+app.use(express.static("public"));
+app.use(routes);
+app.listen(app.get("port"), function() {
+    console.log("Server started on port " + app.get("port"));
+});
